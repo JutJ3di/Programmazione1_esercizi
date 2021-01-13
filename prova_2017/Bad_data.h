2c3347f1c4f27ee18fbb97c2238ddf3d396ff6f7
Emanuele <55718523+JutJ3di@users.noreply.github.com>
2021-01-13 10:38:15 +0100

    Create Bad_data.h

---
+++ prova_2017/Bad_data.h
@@ -0,0 +1,13 @@
+#ifndef ECCEZIONE
+#define ECCEZIONE
+#include<string>
+using namespace std;
+class Errore{
+	
+	public:
+		
+		string errore;
+		Errore(string errore){this->errore= errore;}
+};
+
+#endif
